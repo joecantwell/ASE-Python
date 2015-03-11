49f0a0c6e976c769b379184aae4d176a3981ceac
Joe Cantwell <joe.cantwell@gmail.com>
2015-03-11 16:35:28 +0000

    add and search works

--- contact_client.py
+++ contact_client.py
@@ -3,25 +3,36 @@ import sys
 
 prompt = '> '
 
-def addContact():
+def addContact(stub):
 	contact = addressbook_pb2.Contact()
 	contact.name = raw_input("Enter Name: ")
 	contact.id = int(raw_input("Enter Id Number: "))
 	contact.email = raw_input("Enter Email: ")	
-	with addressbook_pb2.early_adopter_create_ContactService_stub('localhost', 50052) as stub:
-		response = stub.AddContact(contact, 10)
-		print response.msg
+	response = stub.AddContact(contact, 10)
+	print response.msg
 
-while True:
-	print """
-	What do you want to do?
-	A - add a contact
-	S - query a contact
-	Q - quit
-	"""
-	action = str(raw_input(prompt))
-	if (action == 'A' or action == 'a'):
-		addContact()
-	else:
-		break
+def queryContact(searchValue, stub):
+		response = stub.QueryContactName(addressbook_pb2.QueryReq(name =searchValue), 10)
+		print response
+		return
+
+
+with addressbook_pb2.early_adopter_create_ContactService_stub('localhost', 50052) as stub:
+
+	while True:
+		print """
+		What do you want to do?
+		A - add a contact
+		S - query a contact
+		Q - quit
+		"""
+		action = str(raw_input(prompt))
+		if (action == 'A' or action == 'a'):
+			addContact(stub)
+		elif(action == 'S' or action == 's'):
+			print "Enter Name to Search!"
+			searchValue = str(raw_input(prompt))
+			queryContact(searchValue, stub)
+		else:
+			break
 
